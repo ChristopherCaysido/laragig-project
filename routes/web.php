bd5a5ef6c137a83a81c075eb345575d792029293
KatsuKira <christopher.caysido@gmail.com>
2024-06-27 18:42:34 +0800

    Added a Route that GET listings
    Also provided a view for the listings called listings.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
-Route::get('/', function () {
-    return view('welcome');
+// All Listing
+
+Route::get('/', function() {
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
 });
 
 
